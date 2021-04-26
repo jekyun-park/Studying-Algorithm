868c7df750a8b8624f9db24e37534474b4a4b23b
jegyun-Park <parkjekyun@gmail.com>
2021-04-26 18:50:12 +0900

    01 유효한 팰린드롬 uploaded

---
+++ leetcode/125.py
@@ -0,0 +1,55 @@
+# 125. Valid Palindrome
+
+# 1 List
+
+import collections
+import re
+
+
+class Solution:
+
+    def isPalindrome(self, s: str) -> bool:
+        strs = []
+
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+            else:
+                pass
+
+        while len(strs) > 1:
+            if strs.pop(0) != strs.pop():
+                return False
+
+        return True
+
+
+class Solution2:
+
+    def isPalindrome(self, s: str) -> bool:
+
+        strs: Deque = collections.deque()
+
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+            else:
+                pass
+
+        while len(strs) > 1:
+            if strs.popleft() != strs.pop():
+                return False
+
+        return True
+
+# 3 Slicing
+
+
+class Solution3:
+
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+
+        s = re.sub('[^a-z0-9]', '', s)
+
+        return s == s[::-1]
